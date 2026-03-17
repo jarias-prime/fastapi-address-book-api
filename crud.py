ea042b1ad6276360ebf9f88ed25dc0c14f4c3e55
John Arias <jarias.prime@gmail.com>
2026-03-17 22:57:42 +0800

    Feat: Set Database table and CRUD

---
+++ crud.py
@@ -0,0 +1,37 @@
+from sqlalchemy.orm import Session
+import models
+
+def create_address(db: Session, address):
+    db_address = models.Address(**address.dict())
+    db.add(db_address)
+    db.commit()
+    db.refresh(db_address)
+
+    return db_address
+
+def get_addresses(db: Session):
+    return db.query(models.Address).all()
+
+def update_address(db: Session, address_id: int, updated_data):
+    address = db.query(models.Address).filter(models.Address.id == address_id).first()
+    
+    if not address:
+        return None
+    for key, value in updated_data.dict().items():
+        setattr(address, key, value)
+   
+    db.commit()
+    db.refresh(address)
+
+    return address
+
+def delete_address(db: Session, address_id: int):
+    address = db.query(models.Address).filter(models.Address.id == address_id).first()
+    
+    if not address:
+        return None
+
+    db.delete(address)
+    db.commit()
+    
+    return address
